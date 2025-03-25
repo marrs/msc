858fe470879ced18292c62c1cd53c1bb9f19bb25
David Marrs <david@marrsbytes.com>
2025-03-25 17:16:16 +0000

    Refactor components to use d3 hook.

--- web/src/Chart.jsx
+++ web/src/Chart.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3'; 
+import { useD3 } from './hooks';
 import D3Element from './d3element';
 
 const theme = {
@@ -7,57 +8,52 @@ const theme = {
 };
 
 export const BarChart = ({xData, yData, yMax}) => {
-    const ref = useRef();
+  const ref = useD3(svg => {
     const datalen = xData.length;
 
-    useEffect(() => {
-        const svg = d3.select(ref.current)
-            .attr('width', '100vw')
-            .attr('height', '600px')
-            .attr('viewBox', `0, 0, ${datalen}, ${yMax}`)
-            .attr('preserveAspectRatio', 'none')
+    svg.attr('width', '100vw')
+      .attr('height', '600px')
+      .attr('viewBox', `0, 0, ${datalen}, ${yMax}`)
+      .attr('preserveAspectRatio', 'none')
 
-        const update = svg.selectAll('rect').data(yData);
+    const update = svg.selectAll('rect').data(yData);
 
-        const enter = update.enter()
-            .append('rect')
-            .attr('x', (val, idx) => idx)
-            .attr('y', (val) => yMax - val)
-            .attr('width', 1)
-            .attr('height', (val) => val)
-            .attr('fill', theme.fill)
+    const enter = update.enter()
+      .append('rect')
+      .attr('x', (val, idx) => idx)
+      .attr('y', (val) => yMax - val)
+      .attr('width', 1)
+      .attr('height', (val) => val)
+      .attr('fill', theme.fill)
 
-        update.merge(enter).transition().duration(1000)
-            .attr('x', (val, idx) => idx)
-            .attr('y', (val) => yMax - val)
-            .attr('width', 1)
-            .attr('height', (val) => val)
+    update.merge(enter).transition().duration(1000)
+      .attr('x', (val, idx) => idx)
+      .attr('y', (val) => yMax - val)
+      .attr('width', 1)
+      .attr('height', (val) => val)
 
-        update.exit().transition().duration(1000)
-            .attr('y', yMax)
-            .attr('height', 0)
-    });
+    update.exit().transition().duration(1000)
+      .attr('y', yMax)
+      .attr('height', 0)
+  });
 
-    return <D3Element ref={ref} />
+  return <D3Element ref={ref} />
 }
 
 export const LineChart = ({xData, yData, yMax}) => {
-    const ref = useRef();
+  const ref = useD3(svg => {
     const datalen = xData.length;
     const line = d3.line().x((val, idx) => idx).y((val) => yMax - val)
+    svg.attr('width', '100vw')
+      .attr('height', '600px')
+      .attr('viewBox', `0, 0, ${datalen}, ${yMax}`)
+      .attr('preserveAspectRatio', 'none')
+    svg.append('path')
+      .attr('fill', 'none')
+      .attr('stroke', theme.fill)
+      .attr('stroke-width', 0.2)
+      .attr('d', line(yData))
+  });
 
-    useEffect(() => {
-        const svg = d3.select(ref.current)
-            .attr('width', '100vw')
-            .attr('height', '600px')
-            .attr('viewBox', `0, 0, ${datalen}, ${yMax}`)
-            .attr('preserveAspectRatio', 'none')
-        svg.append('path')
-            .attr('fill', 'none')
-            .attr('stroke', theme.fill)
-            .attr('stroke-width', 0.2)
-            .attr('d', line(yData))
-    });
-
-    return <D3Element ref={ref} />
+  return <D3Element ref={ref} />
 }

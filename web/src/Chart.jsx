f597ffb5538655cba0e5e4191d223615e5fa42ec
David Marrs <david@marrsbytes.com>
2025-03-25 21:16:04 +0000

    WIP: Add axes to line chart

--- web/src/Chart.jsx
+++ web/src/Chart.jsx
@@ -8,6 +8,11 @@ const theme = {
     anim_rate: 1000,
 };
 
+const width = 1000;
+const height = 800;
+const margin = 40;
+
+
 function Date_from_date(date) {
   let [dt, mth, yr] = date.split('/');
   return new Date(+yr, +mth, +dt);
@@ -24,25 +29,21 @@ export const BarChart = (props) => {
   const ref = useD3(svg => {
 
     const { yMax, xData, yData } = props;
-    const width = 1000;
-    const height = 800;
-    const margin = 40;
+
+    function x_unit(x) {
+      return x * (width - 2 * margin) / xData.length
+    }
+
+    function y_unit(x) {
+      return x * (height - 2 * margin) / yMax;
+    }
+
 
     svg = init_svg(svg, {
       width: width + margin * 2,
       height: height + margin * 2
     })
 
-    const widthUnit = (width - 2 * margin) / xData.length
-    const heightUnit = (height - 2 * margin) / yMax
-
-    function x_unit(x) {
-      return x * widthUnit;
-    }
-
-    function y_unit(x) {
-      return x * heightUnit;
-    }
 
       /*
     svg.append('g')
@@ -75,14 +76,14 @@ export const BarChart = (props) => {
       .append('rect')
       .attr('x', (val, idx) => margin + x_unit(idx))
       .attr('y', (val) => height - margin - y_unit(val))
-      .attr('width', widthUnit)
+      .attr('width', x_unit(1))
       .attr('height', (val) => y_unit(val))
       .attr('fill', theme.fill)
 
     update.merge(enter).transition().duration(theme.anim_rate)
       .attr('x', (val, idx) => margin + x_unit(idx))
       .attr('y', (val) => height - margin - y_unit(val))
-      .attr('width', widthUnit)
+      .attr('width', x_unit(1))
       .attr('height', (val) => y_unit(val)),
 
     update.exit().transition().duration(theme.anim_rate)
@@ -122,20 +123,60 @@ export const BarChart = (props) => {
 export const LineChart = (props) => {
   const { yMax, xData, yData } = props;
 
+  function x_unit(x) {
+    return x * (width - 2 * margin) / xData.length
+  }
+
+  function y_unit(x) {
+    return x * (height - 2 * margin) / yMax;
+  }
+
   const ref = useD3(svg => {
     const datalen = xData.length;
-    const line = d3.line().x((val, idx) => idx).y((val) => yMax - val)
+    const line = d3.line()
+      .x((val, idx) => margin + x_unit(idx))
+      .y((val) => height - margin - y_unit(val))
 
-    init_svg(svg, props);
-    svg.append('path')
+    svg = init_svg(svg, {
+      width: width + margin * 2,
+      height: height + margin * 2
+    })
+
+    svg.append('g').append('path')
       .attr('fill', 'none')
       .attr('stroke', theme.fill)
       .attr('stroke-width', 0.2)
       .attr('d', line(yData))
+
+    if (xData.length) {
+      const xScale = d3.scaleTime()
+        .domain([Date_from_date(xData.at(0)), Date_from_date(xData.at(-1))])
+        .range([0, width])
+
+      svg
+        .append("g")
+          .attr("transform", `translate(${margin}, ${height - margin})`)
+          .call(d3.axisBottom(xScale).ticks(
+            d3.timeMonth.every(3)).tickSizeOuter(0)
+          );
+    }
+
+    if (yData.length) {
+      const yScale = d3.scaleLinear()
+        .domain([yMax, 0])
+        .range([0, height - margin])
+
+      svg
+        .append("g")
+          .attr("transform", `translate(${margin}, ${0})`)
+          .call(d3.axisLeft(yScale));
+    }
   }, []);
 
   useEffect(() => {
-    const line = d3.line().x((val, idx) => idx).y((val) => yMax - val)
+    const line = d3.line()
+      .x((val, idx) => margin + x_unit(idx))
+      .y((val) => height - margin - y_unit(val))
     d3.select(ref.current).selectAll('path')
       .transition().duration(theme.anim_rate)
       .attr('d', line(yData))

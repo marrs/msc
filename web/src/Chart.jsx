370ca11b8f6786822643998e5b95485f0ea32b81
David Marrs <david@marrsbytes.com>
2025-03-25 16:54:10 +0000

    Animate bar chart.

--- web/src/Chart.jsx
+++ web/src/Chart.jsx
@@ -9,19 +9,33 @@ const theme = {
 export const BarChart = ({xData, yData, yMax}) => {
     const ref = useRef();
     const datalen = xData.length;
+
     useEffect(() => {
         const svg = d3.select(ref.current)
             .attr('width', '100vw')
             .attr('height', '600px')
             .attr('viewBox', `0, 0, ${datalen}, ${yMax}`)
             .attr('preserveAspectRatio', 'none')
-        svg.selectAll('rect')
-            .data(yData)
-            .enter()
+
+        const update = svg.selectAll('rect').data(yData);
+
+        const enter = update.enter()
             .append('rect')
             .attr('x', (val, idx) => idx)
             .attr('y', (val) => yMax - val)
-            .attr('width', 1) .attr('height', (val) => val) .attr('fill', theme.fill);
+            .attr('width', 1)
+            .attr('height', (val) => val)
+            .attr('fill', theme.fill)
+
+        update.merge(enter).transition().duration(1000)
+            .attr('x', (val, idx) => idx)
+            .attr('y', (val) => yMax - val)
+            .attr('width', 1)
+            .attr('height', (val) => val)
+
+        update.exit().transition().duration(1000)
+            .attr('y', yMax)
+            .attr('height', 0)
     });
 
     return <D3Element ref={ref} />
